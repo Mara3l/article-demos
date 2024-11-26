0e5683abce0f5693998ec74a0f47446170f48641
Mara3l <Mara3l@proton.me>
2024-11-26 10:41:03 +0100

    Removing Tests for MongoDB

--- flexconnect/mongodb/tests/flexconnect/test_sample_function.py
+++
@@ -1,9 +0,0 @@
-# (C) 2024 GoodData Corporation
-from flexconnect.sample_function import SampleFlexConnectFunction
-
-
-def test_sample_function1(flexconnect_call_parameters):
-    fun = SampleFlexConnectFunction.create()
-    result = fun.call(flexconnect_call_parameters, None, {})
-
-    assert result is not None
